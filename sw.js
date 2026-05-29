7d3f49bf0722972a90a11b80224540a890fa0195
chrstopher03 <160043279+chrstopher03@users.noreply.github.com>
2026-05-29 11:04:18 -0600

    ya

--- sw.js
+++ sw.js
@@ -1,121 +1,79 @@
-const CACHE_NAME = 'lubri-expres-v2';
+const CACHE_NAME = 'lubri-expres-v3';
 
 const urlsToCache = [
-
-'./',
-'./index.html',
-'./funcion.js',
-'./manifest.json',
-'./logo.jpeg',
-'./30.jpeg',
-'./01.jpeg',
-'./75.jpeg',
-'./09.jpeg'
-
+  './',
+  './index.html',
+  './funcion.js',
+  './manifest.json',
+  './logo.jpeg',
+  './30.jpeg',
+  './01.jpeg',
+  './75.jpeg',
+  './09.jpeg'
 ];
 
 /* =========================
-INSTALAR
+INSTALL
 ========================= */
-
 self.addEventListener('install', event => {
+  self.skipWaiting();
 
-console.log('Service Worker instalado');
-
-event.waitUntil(
-
-caches.open(CACHE_NAME)
-.then(cache => {
-
-return cache.addAll(urlsToCache);
-
-})
-
-);
-
-self.skipWaiting();
-
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(async cache => {
+      for (const url of urlsToCache) {
+        try {
+          await cache.add(url);
+        } catch (err) {
+          console.log('No se pudo cachear:', url);
+        }
+      }
+    })
+  );
 });
 
 /* =========================
-ACTIVAR
+ACTIVATE
 ========================= */
-
 self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
 
-console.log('Service Worker activado');
-
-event.waitUntil(
-
-caches.keys().then(cacheNames => {
-
-return Promise.all(
-
-cacheNames.map(cache => {
-
-if(cache !== CACHE_NAME){
-
-return caches.delete(cache);
-
-}
-
-})
-
-);
-
-})
-
-);
-
-self.clients.claim();
-
+  self.clients.claim();
 });
 
 /* =========================
-FETCH
+FETCH (ESTABLE + SEGURO)
 ========================= */
-
 self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(networkResponse => {
+        return caches.open(CACHE_NAME).then(cache => {
+          try {
+            cache.put(event.request, networkResponse.clone());
+          } catch (e) {
+            console.log('Cache put error:', e);
+          }
+          return networkResponse;
+        });
+      })
+      .catch(() => {
+        return caches.match(event.request).then(cached => {
+          if (cached) return cached;
 
-event.respondWith(
-
-caches.match(event.request)
-.then(response => {
-
-if(response){
-
-return response;
-
-}
-
-return fetch(event.request)
-.then(networkResponse => {
-
-return caches.open(CACHE_NAME)
-.then(cache => {
-
-cache.put(
-event.request,
-networkResponse.clone()
-);
-
-return networkResponse;
-
-});
-
-});
-
-})
-.catch(() => {
-
-if(event.request.mode === 'navigate'){
-
-return caches.match('./index.html');
-
-}
-
-})
-
-);
-
+          if (event.request.mode === 'navigate') {
+            return caches.match('./index.html');
+          }
+        });
+      })
+  );
 });
