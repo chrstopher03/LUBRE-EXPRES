400c82e53f0601784579c5ab2b447f1f4fcc7dd6
chrstopher03 <160043279+chrstopher03@users.noreply.github.com>
2026-06-01 17:23:34 -0600

    ya

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 VERSION
 ========================= */
 
-const CACHE_VERSION = 'v3';
+const CACHE_VERSION = 'v4';
 
 const CACHE_NAME =
 'lubri-expres-' + CACHE_VERSION;
@@ -13,16 +13,20 @@ ARCHIVOS
 
 const urlsToCache = [
 
-'./',
-'./index.html',
-'./funcion.js',
-'./manifest.json',
+'/',
+'/index.html',
+'/style.css',
+'/funcion.js',
+'/manifest.json',
 
-'./logo.jpeg',
-'./30.jpeg',
-'./01.jpeg',
-'./75.jpeg',
-'./09.jpeg'
+'/logo.jpeg',
+'/icon-192.png',
+'/icon-512.png',
+
+'/30.jpeg',
+'/01.jpeg',
+'/75.jpeg',
+'/09.jpeg'
 
 ];
 
@@ -107,12 +111,17 @@ self.clients.claim();
 
 /* =========================
 FETCH
-(Network First)
 ========================= */
 
 self.addEventListener(
 'fetch',
 event => {
+
+if(
+event.request.method !== 'GET'
+){
+return;
+}
 
 event.respondWith(
 
@@ -120,7 +129,7 @@ fetch(event.request)
 
 .then(response => {
 
-const clone =
+const responseClone =
 response.clone();
 
 caches.open(CACHE_NAME)
@@ -128,7 +137,7 @@ caches.open(CACHE_NAME)
 
 cache.put(
 event.request,
-clone
+responseClone
 );
 
 });
@@ -157,7 +166,7 @@ event.request.mode ===
 ){
 
 return caches.match(
-'./index.html'
+'/index.html'
 );
 
 }
@@ -172,7 +181,7 @@ return caches.match(
 );
 
 /* =========================
-ACTUALIZACION FORZADA
+ACTUALIZAR
 ========================= */
 
 self.addEventListener(
@@ -180,8 +189,7 @@ self.addEventListener(
 event => {
 
 if(
-event.data &&
-event.data.action ===
+event.data?.action ===
 'skipWaiting'
 ){
 
